156bc80288e191d7c24a83d8d31a5c8cb753439f
lvnpevensie <32437174+lvnpevensie@users.noreply.github.com>
2021-01-10 07:29:29 +0800

    Update SearchCommand

--- Contact-Management-System-original4/Contact-Management-System-original/Contact-Management-System-master/src/contactmanagementsoftware/SearchCommand.java
+++ Contact-Management-System-original4/Contact-Management-System-original/Contact-Management-System-master/src/contactmanagementsoftware/SearchCommand.java
@@ -7,7 +7,6 @@ import javax.swing.JOptionPane;
 //Button3
 public class SearchCommand implements Command {
 
-	MUI mui;
 	Search search;
 
 	public SearchCommand(Search search) {
@@ -59,81 +58,5 @@ class Search {
         }
         mui.getDetails().setText(s);
         
-            
-		
-            /*
-		ArrayList<ArrayList<Acquaintances>> a = mui.getA();
-		String str = mui.getStr();
-		
-		String s = "<html> <b>Search results:</b><br>Found!<br><br>Acquaintance Details: <br>";
-		int j = 0;
-		for(int i = 0; i < a.get(0).size(); i++){
-			if(a.get(0).get(i).getName().matches(str)){
-				j++;
-				PersonalFriends perF = (PersonalFriends)a.get(0).get(i);
-				if(j==1){
-					s = s.concat("<br>I. Personal Friends<br>");
-				}
-				s = s.concat(j + ". Name: " + perF.getName() + "<br>");
-				s = s.concat("Mobile No: " + perF.getMobileNo() + "<br>");
-				s = s.concat("Email: " + perF.getEmail() + "<br>");
-				s = s.concat("Specific events: " + perF.getEvents() + "<br>");
-				s = s.concat("First Acquaintance context: " + perF.getAContext() + "<br>");
-				s = s.concat("First Acquaintance date: " + perF.getADate() + "<br>");
-			}
-		}
-		j = 0;
-		for(int i = 0; i < a.get(1).size(); i++){
-			if(a.get(1).get(i).getName().matches(str)){
-				j++;
-				Relatives rel = (Relatives)a.get(1).get(i);
-				if(j==1){
-					s = s.concat("<br>II. Relatives<br>");
-				}
-				s = s.concat(j + ". Name: " + rel.getName() + "<br>");
-				s = s.concat("Mobile No: " + rel.getMobileNo() + "<br>");
-				s = s.concat("Email: " + rel.getEmail() + "<br>");
-				s = s.concat("Relatives Birthday: " + rel.getBDate() + "<br>");
-				s = s.concat("Last met date: " + rel.getLDate() + "<br>");
-			}
-		}
-		j = 0;
-		for(int i = 0; i < a.get(2).size(); i++){
-			if(a.get(2).get(i).getName().matches(str)){
-				j++;
-				ProfessionalFriends proF = (ProfessionalFriends)a.get(2).get(i);
-				if(j==1){
-					s = s.concat("<br>III. Professional Friends<br>");
-				}
-				s = s.concat(j + ". Name: " + proF.getName() + "<br>");
-				s = s.concat("Mobile No: " + proF.getMobileNo() + "<br>");
-				s = s.concat("Email: " + proF.getEmail() + "<br>");
-				s = s.concat("Common Interests: " + proF.getCommonInterests() + "<br>");
-			}
-		}
-		j = 0;
-		for(int i = 0; i < a.get(3).size(); i++){
-			if(a.get(3).get(i).getName().matches(str)){
-				j++;
-				CasualAcquaintances ca = (CasualAcquaintances)a.get(3).get(i);
-				if(j==1){
-					s = s.concat("<br>IV. Casual Acquaintances<br>");
-				}
-				s = s.concat(j + ". Name: " + ca.getName() + "<br>");
-				s = s.concat("Mobile No: " + ca.getMobileNo() + "<br>");
-				s = s.concat("Email: " + ca.getEmail() + "<br>");
-				s = s.concat("First met location & time: " + ca.getWhenWhere() + "<br>");
-				s = s.concat("First met circumstances: " + ca.getCircumstances() + "<br>");
-				s = s.concat("Other useful information: " + ca.getOtherInfo() + "<br>");
-			}
-		}
-		if(s.matches("<html> <b>Search results:</b><br>Found!<br><br>Acquaintance Details: <br>")){
-			s  = "<html>No result found</html>";
-		}
-		else{
-			s = s.concat("</html>");
-		}
-		mui.getDetails().setText(s);
-*/
 	}
 }
